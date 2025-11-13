5f86c47f41f1e1d0ff23bf44278a496f8c4b6527
edimar zambrano <epzambrano.18@est.ucab.edu.ve>
2025-11-13 08:32:51 -0400

    entrega1 login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,16 +6,21 @@ import { LoginComponent } from './pages/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 import { DashboardComponent } from './pages/dashboardAdmin/dashboard.component';
 import { AdminGuard } from './guards/admin.guard';
-import { PagosComponent } from './pages/pagos/pagos.component'; // Agregar esta línea
-import { RegistroComponent } from './pages/registro/registro.component'; 
-
-
+import { PagosComponent } from './pages/pagos/pagos.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'registro', component: RegistroComponent },
   { 
     path: '', 
+    redirectTo: 'login',  
+    pathMatch: 'full' 
+  },
+  { 
+    path: '', 
+    component: LoginComponent 
+  },
+  { 
+    path: 'servicios', 
     component: ServiciosComponent,
     canActivate: [AuthGuard]
   },
@@ -27,12 +32,19 @@ export const routes: Routes = [
   { 
     path: 'pagos', 
     component: PagosComponent, 
-    canActivate: [AdminGuard] // Solo admin puede ver pagos
+    canActivate: [AdminGuard]
   },
   { 
     path: 'especialidad/:id', 
     component: EspecialidadComponent,
     canActivate: [AuthGuard]
   },
-  { path: '**', redirectTo: '' }
+  { 
+    path: 'registro', 
+    component: RegistroComponent 
+  },
+  { 
+    path: '**', 
+    redirectTo: 'login'  // ← Redirige a la raíz (login)
+  }
 ];
